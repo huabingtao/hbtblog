40358d375a7a30c374e0d2f7a6680e37583efee8
huabingtao <1273621932@qq.com>
2020-02-29 13:28:24 +0800

    fix  base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,34 @@
 module.exports = {
   title: "Hbt'blog",
-  base: '/blog/',
-  description: 'Vue React Node Koa2',
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
-  port: '2020',
-  tags: '/tags',
+  base: "/",
+  description: "Vue React Node Koa2",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  port: "2020",
+  tags: "/tags",
   markdown: {
     lineNumbers: true
   },
-  plugins: ['@vuepress/active-header-links', '@vuepress/back-to-top'],
+  plugins: ["@vuepress/active-header-links", "@vuepress/back-to-top"],
   themeConfig: {
-    logo: '/logo.png',
+    logo: "/logo.png",
     nav: [
       {
-        text: 'GitHub',
-        link: 'https://github.com/huabingtao',
-        target: '_blank'
+        text: "GitHub",
+        link: "https://github.com/huabingtao",
+        target: "_blank"
       }
     ],
     sidebarDepth: 6,
     displayAllHeaders: false,
-    sidebar: ['/node/diary'],
+    sidebar: ["/node/diary"],
     smoothScroll: true // 页面滚动
   },
-  lastUpdated: 'Last Updated', // string | boolean
+  lastUpdated: "Last Updated", // string | boolean
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': 'path/to/some/dir'
+        "@alias": "path/to/some/dir"
       }
     }
   }
-}
+};
