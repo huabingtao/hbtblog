9ce8ff57c074f36c62e2d79984385cc9928e8693
huabingtao <1273621932@qq.com>
2021-02-05 23:37:20 +0800

    build

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env sh
-###
- # @Author: huabingtao
- # @Date: 2021-02-02 22:38:46
- # @LastEditTime: 2021-02-02 23:11:10
- # @LastEditors: huabingtao
- # @Description: 
- # @FilePath: /vue-press/deploy.sh
- # @可以输入预定的版权声明、个性签名、空行等
-### 
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -21,7 +12,7 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -29,7 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push origin master:gh-pages
-# git push -f https://${access_token}@github.com/huabingtao/hbtblog.git master:gh-pages
+git push -f git@github.com:huabingtao/hbtblog.git master:gh-pages
 
 cd -
